58f86905eeb0b0c8a20bac5e32e88006e064d64a
Tommy Ernsund <13759796+ern1@users.noreply.github.com>
2020-10-11 00:06:35 +0200

    as a last resort, I sort of had to fix problem

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ def is_sorted(seq):
     return all(x <= seq[i + 1] for i, x in enumerate(seq[:-1]))
 
 def test_ss(seq, f1, f2):
-    print(f"{seq}\nSorted: {is_sorted(l)}\n") # not sorted as you would expect
-    ss.sort_sort(seq, f1, f2)                  # call sort function
-    print(f"{seq}\nSorted: {is_sorted(l)}\n") # and just like that, it is still not sorted
+    print(f"{seq}\nSorted: {is_sorted(seq)}\n")       # not sorted as you would expect
+    seq_ss = ss.sort_sort(seq, f1, f2)                # call sort function
+    print(f"{seq_ss}\nSorted: {is_sorted(seq_ss)}\n") # and just like that, it is still not sorted
 
 if __name__ == '__main__':
     l = [1, 2, 3, 4923847293874, 9, 10]
